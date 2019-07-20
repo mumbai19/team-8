645d2d05b4fe1396349fe0740baa8cb7e94b024a
Gaurav <gauravsahu.gs.98@gmail.com>
2019-07-21 01:56:38 +0530

    Studentupdate and assessments added

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
-from .models import Mentor, Student
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Mentor, Student, Attendance, Evaluate
+from django.contrib.auth.models import User
 # Create your views here.
 
 # get mentor details
@@ -32,3 +33,44 @@ def login_user(request):
         else:
             return render(request, 'main/.html', {'error_message': 'Invalid login'})
     return render(request, 'main/.html')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User(username = username, password = password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'main/.html')
+    return render(request, 'main/.html')
+
+def getAttendance(request, student_id):
+    attendance = Attendance.objects.get(student_id=student_id)
+    context = {
+        'attendance': attendance
+    }
+    return render(request, 'main/.html', context)
+
+def getassessments(request, student_id):
+    assessments = Evaluate.objects.get(student_id=student_id)
+    context = {
+        'assessments': assessments
+    }
+    return render(request, 'main/.html', context)
+
+def updateStudentInfo(request, student_id):
+    if request.method == "POST":
+        student_name = request.POST['student_name']
+        parent_name = request.POST['parent_name']
+        phone_no = request.POST['phone_no']
+        standard = request.POST['standard']
+        address = request.POST['address']
+        mentor = request.POST['mentor']
+
+        Student.objects.get(student_id=student_id).update(student_name=student_name, parent_name=parent_name, phone_no=phone_no, standard=standard, address=address, mentor=mentor)
+        return render(request, 'main/.html')
+    return render(request, 'main/.html')
+
