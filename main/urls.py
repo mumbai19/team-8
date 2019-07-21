c05d675fcdf3d897eb53523e0b027e5f95bf5ea4
Gaurav <gauravsahu.gs.98@gmail.com>
2019-07-21 08:26:08 +0530

    Integration

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     url(r'^students/$', views.studentDetails, name='students'),
     url(r'^attendance/$', views.getAttendance, name='attendance'),
     url(r'^assessments/$', views.getAssessments, name='assessments'),
-    url(r'^studentupdate/$', views.getUpdate, name='studentupdate'),
-    url(r'^studentsform/$', views.updateform, name='studentsform')
+    url(r'^addassessment/$', views.addAssessment, name='addassessment'),
+    url(r'^studentupdate/$', views.updateStudentInfo, name='studentupdate'),
+    url(r'^studentsform/$', views.updateform, name='studentsform'),
+    url(r'^addactivity/$', views.activitySubmit, name='addactivity'),
+    url(r'^getactivity/$', views.getActivity, name='getactivity')
     # url(r'^stars/$', views.getStars, name='stars'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Mentor, Student, Attendance, Evaluate
+from .models import Mentor, Student, Attendance, Evaluate, Saving, Activity
 from django.contrib.auth.models import User
 # Create your views here.
 
@@ -52,29 +52,77 @@ def register(request):
                 return render(request, 'main/.html')
     return render(request, 'main/.html')
 
-def getAttendance(request, student_id):
-    attendance = Attendance.objects.get(student_id=student_id)
-    return render(request, 'main/Attendance.html', attendance)
+def getAttendance(request):
+    attendance = Attendance.objects.all()
+    for i in attendance:
+        i.name = Student.objects.get(student_id = i.student_id).student_name
+        i.savings = Saving.objects.get(student_id = i.student_id).savings
+        i.saving_date = Saving.objects.get(student_id = i.student_id).saving_date
+    return render(request, 'main/Attendance.html', {'attendance': attendance})
 
-def getAssessments(request, student_id):
-    assessments = Evaluate.objects.get(student_id=student_id)
-    context = {
-        'assessments': assessments
-    }
-    return render(request, 'main/.html', context)
+def getAssessments(request):
+    assessments = Evaluate.objects.all()
+    for i in assessments:
+        i.name = Student.objects.get(student_id = i.student_id).student_name
+    return render(request, 'main/getAssessment.html', {'evaluate': assessments})
 
-def updateStudentInfo(request, student_id):
+def addAssessment(request):
+    if request.method == "POST":
+        student_id = Student.objects.get(student_name=request.POST['student_name'])
+        assess_name = request.POST['assess_name']
+        assess_date = request.POST['assess_date']
+        assess_level = request.POST['assess_level']
+        mentor = Mentor.objects.get(mentor_name=request.POST['mentor'])
+
+        evaluate = Evaluate(student_id=student_id, assess_name=assess_name, assess_date=assess_date, assess_level=assess_level, mentor_id=mentor)
+        evaluate.save()
+        evaluate = Evaluate.objects.all()
+
+        for i in evaluate:
+            i.name = Student.objects.get(student_id = i.student_id).student_name
+        return render(request, 'main/getAssessment.html', {'evaluate': evaluate})
+    evaluate = Evaluate.objects.all()
+    for i in evaluate:
+        i.name = Student.objects.get(student_id = i.student_id).student_name
+    return render(request, 'main/getAssessment.html', {'evaluate': evaluate})
+
+def updateStudentInfo(request):
     if request.method == "POST":
         student_name = request.POST['student_name']
         parent_name = request.POST['parent_name']
         phone_no = request.POST['phone_no']
         standard = request.POST['standard']
         address = request.POST['address']
-        mentor = request.POST['mentor']
+        mentor = Mentor.objects.get(mentor_name=request.POST['mentor'])
 
-        Student.objects.get(student_id=student_id).update(student_name=student_name, parent_name=parent_name, phone_no=phone_no, standard=standard, address=address, mentor=mentor)
-        return render(request, 'main/.html')
-    return render(request, 'main/.html')
+        student = Student(student_name=student_name, parent_name=parent_name, phone_no=phone_no, standard=standard, address=address, mentor=mentor)
+        student.save()
+        students = Student.objects.all()
+        return render(request, 'main/studentDetails.html', {'students': students})
+    students = Student.objects.all()
+    return render(request, 'main/studentDetails.html', {'students': students})
 
 def updateform(request):
     return render(request, 'main/studentUpdate.html')
+
+def activitySubmit(request):
+    if request.method == "POST":
+        act_name = request.POST['activity_name']
+        act_des = request.POST['activity_description']
+        the = request.POST['theme']
+        mentor = Mentor.objects.get(mentor_name=request.POST['mentor'])
+        order = Activity.objects.create(activity_name=act_name, activity_description=act_des, theme=the, mentor_id=mentor)
+        activities = Activity.objects.all()
+        for i in activities:
+            i.name = Mentor.objects.get(mentor_id = i.mentor_id).mentor_name
+        return render(request, 'main/activityreport.html', {'activities': activities})
+    activities = Activity.objects.all()
+    for i in activities:
+        i.name = Mentor.objects.get(mentor_id = i.mentor_id).mentor_name
+    return render(request, 'main/activityreport.html', {'activities': activities})
+
+def getActivity(request):
+    activities = Activity.objects.all()
+    for i in activities:
+        i.name = Mentor.objects.get(mentor_id = i.mentor_id).mentor_name
+    return render(request, 'main/activityreport.html', {'activities': activities})
